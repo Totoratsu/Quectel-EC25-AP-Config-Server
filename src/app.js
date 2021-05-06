83834778dac4476b699053e843a67cd9637638d2
Totoratsu <mayor.daniel.18@gmail.com>
2021-05-06 18:05:50 -0500

    Added morgan

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const routes = require('./routes');
+const morgan = require('morgan');
 
 const app = express();
 const {
@@ -9,6 +10,7 @@ const {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(morgan('dev'));
 app.set('views', path.join('src', 'views'));
 app.set('view engine', 'pug');
 
